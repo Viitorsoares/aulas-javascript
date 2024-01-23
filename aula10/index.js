d1de80bcdc4994563fc1b11f7c190886152552bf
Viitorsoares <viitorgab.soares@gmail.com>
2024-01-23 16:39:51 -0300

    Aula 10 Javascript finalizada

--- aula10/index.js
+++ aula10/index.js
@@ -1,7 +1,29 @@
 const input = document.getElementById('input')
 
+// Função que aparece a frase no input 
 document.getElementById('value').addEventListener('click', function () {
     input.value = 'Olá, Mundo!'
+})
 
-    
+// Função que muda o input para radio
+document.getElementById('type').addEventListener('click', function () {
+    input.setAttribute('type', 'radio')
+})
+
+// Função que aparece o placeholder
+document.getElementById('placeholder').addEventListener('click', function () {
+    input.placeholder = 'Dígite algo...'
+})
+
+// Função que alterna um valor do input    (A propriedade 'disabled' é booleano )
+document.getElementById('disable').addEventListener('click', function () {
+    input.setAttribute('disabled', !input.disabled)
+})
+
+// Função de data 
+document.getElementById('data').addEventListener('click', function () {
+    const data = input. dataset.something   // propriedade 'dataset' referencia a todos os atibutos 'data' do HTML
+    alert("O valor do atributo data-something é: " + data)
+    input.dataset.something = 'Algum outro valor'
+    alert("O valor do atributo data-something agora é: " + input.dataset.something)
 })
